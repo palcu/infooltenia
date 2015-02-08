ac5f41e56ec6de8ce3a9fb547631d35d2a75b2d4
andrei14vl <andrei14vl@gmail.com>
2015-02-08 15:44:26 +0200

    Update lista + 'sesiune'

--- lipsa/lipsa_and.cpp
+++ lipsa/lipsa_and.cpp
@@ -1,14 +1,16 @@
 #include <fstream>
 #include <unordered_map>
+#include <vector>
 using namespace std;
 
 ifstream f("lipsa.in");
 ofstream g("lipsa.out");
 
 unordered_map <int, int> missing_number;
+vector <int> max_count_list;
 
 int main() {
-    int N, M, max_count = 0, max_count_val;
+    int N, M, max_count = 0;
 
     f >> N >> M;
     for (int it = 1; it <= M; ++it) {
@@ -21,10 +23,18 @@ int main() {
         }
         /* Sum is the missing number */
         int temp_missing_nr_count = ++missing_number[Sum];
-        if (temp_missing_nr_count > max_count)
-            max_count = temp_missing_nr_count, max_count_val = Sum;
+        if (temp_missing_nr_count >= max_count) {
+            if (temp_missing_nr_count > max_count) {
+                max_count = temp_missing_nr_count;
+                max_count_list.clear();
+            }
+            max_count_list.push_back(Sum);
+        }
     }
 
-    g << max_count_val << "\n";
+    g << max_count << " " << max_count_list.size() << "\n";
+    for (int list_it = 0; list_it < max_count_list.size(); ++list_it)
+        g << max_count_list[list_it] << " ";
+    g << "\n";
     return 0;
 }
