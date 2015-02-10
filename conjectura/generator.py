8f6672c48fdd1863c5279fc7709b3cfa5636ad42
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-02-10 19:20:28 +0200

    Add generator for conjectura

---
+++ conjectura/generator.py
@@ -0,0 +1,19 @@
+from random import shuffle
+
+
+stream = open('numere.txt')
+numere = []
+for line in stream:
+  elements = line.split()
+  numere.append({'id': int(elements[0]),
+                 'iterations': int(elements[1]),
+                 'length': int(elements[2])})
+
+MAX_INT = 2147483647
+MAX_LEN_INT = len(str(MAX_INT)) - 1
+
+n = 30
+
+shuffle(numere)
+for i in range(n):
+  print numere[i]['id']
