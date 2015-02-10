6500bc2682aeecb29c79a9ee9eff4e33439c2b49
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-02-10 20:25:53 +0200

    Add verifica_diff script

---
+++ conjectura/teste/verifica_diff.py
@@ -0,0 +1,20 @@
+from sh import cp, rm, diff
+import sh
+import os
+
+SURSA_VERIFICATA = 'conjectura-inturi.cpp'
+
+cp('../' + SURSA_VERIFICATA, '.')
+os.system('g++ ' + SURSA_VERIFICATA)
+
+filename = 'grader_test'
+for i in range(1, 11):
+  print 'Testul ', i
+  cp(filename + str(i) + '.in', 'conjectura.in')
+  os.system('./a.out')
+  print diff('conjectura.out', filename + str(i) + '.ok')
+
+for extension in ['in', 'out']:
+  rm('conjectura.' + extension)
+rm(SURSA_VERIFICATA)
+rm('a.out')
