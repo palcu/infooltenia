bf4e029d5e036cc3c31139e6a4c33f6665d65746
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-02-10 23:40:11 +0200

    Sorteaza chestii in lipsa_and.cpp

--- lipsa/teste/verifica_diff.py
+++ lipsa/teste/verifica_diff.py
@@ -2,7 +2,7 @@ from sh import cp, rm, diff, ErrorReturnCode
 import sh
 import os
 
-SURSA_VERIFICATA = 'lipsa-sortare.cpp'
+SURSA_VERIFICATA = 'lipsa_and.cpp'
 
 cp('../' + SURSA_VERIFICATA, '.')
 os.system('g++ ' + SURSA_VERIFICATA)
