ae5e6bdc37570fffe69bbd970fa06e6fa1364d5a
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-02-12 01:40:07 +0200

    Fix grader_test1 de la conuri

--- conuri/teste/verifica_diff.py
+++ conuri/teste/verifica_diff.py
@@ -2,21 +2,19 @@ from sh import cp, rm, diff, ErrorReturnCode
 import sh
 import os
 
-SURSA_VERIFICATA = 'conuri-n2.cpp'
+SURSA_VERIFICATA = 'conuri-robert.cpp'
 
 cp('../' + SURSA_VERIFICATA, '.')
 os.system('g++ ' + SURSA_VERIFICATA)
 
-filename = 'conuri'
-for i in range(0, 10):
-  if (i == 2):
-    continue
+filename = 'grader_test'
+for i in range(1, 11):
   print 'Testul ', i
-  cp(str(i) + '-' + filename + '.in', 'conuri.in')
-  os.system('( cmdpid=$BASHPID; (sleep 10; kill $cmdpid) & exec ./a.out )')
-  print diff('conuri.out', str(i) + '-' + filename + '.ok', _ok_code=[0, 1])
+  cp(filename + str(i) + '.in', 'conuri.in')
+  os.system('./a.out')
+  print diff('conuri.out', filename + str(i) + '.ok', _ok_code=[0, 1])
 
 for extension in ['in', 'out']:
-  rm('conjectura.' + extension)
+  rm('conuri.' + extension)
 rm(SURSA_VERIFICATA)
 rm('a.out')
